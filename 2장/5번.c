a46efc71d33640945475933a356f05f5c09dd036
ChoHeeseok <hantecude123@gmail.com>
2024-04-08 14:45:31 +0900

    Create 5번.c

---
+++ 2장/5번.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("The double quotation mark is \".\n");
+    printf("The single quotation mark is \'.\n");
+    printf("Character A is \'A\'.\n");
+    printf("To represent a double quotation mark, use the escape sequence \\\".\n");
+    
+	return 0;
+}
