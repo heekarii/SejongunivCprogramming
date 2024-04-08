b58159573cff3414b15ba74d5d27ecd4ee43e1cc
ChoHeeseok <hantecude123@gmail.com>
2024-04-08 14:39:18 +0900

    Create 3번.c

---
+++ 1장/3번.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int x;
+  scanf("%d", &x);
+  printf("%d%d%d%d%d%d\n", x, x, x, x, x, x);
+  printf("%d    %d\n", x, x);
+  printf("%d    %d\n", x, x);
+  printf("%d%d%d%d%d%d\n", x, x, x, x, x, x);
+  
+  return 0;
+}
