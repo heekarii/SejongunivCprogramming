8346eee495f17e2349c184c57a5bf4c88630d84d
ChoHeeseok <hantecude123@gmail.com>
2024-04-08 14:43:33 +0900

    Create 2번.c

---
+++ 2장/2번.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  double x, y;
+  x= 4.2;
+  y= 5.1;
+  printf("first number: %f\n", x);
+  printf("second number: %f\n", y);
+  printf("sum: %f", x+y);
+  return 0;
+}
